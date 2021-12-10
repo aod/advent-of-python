0c4197f156dc0e97d4f3e4a8afa8c069f3e07d6d
Oktay <oktay@yatko.dev>
2021-12-10 12:06:39 +0100

    Solve 2021-3

---
+++ 2021/day03.py
@@ -0,0 +1,59 @@
+from aocd import lines
+
+
+def part1(bit_size=12):
+    numbers = [int(line, 2) for line in lines]
+    gamma = 0
+    epsilon = 0
+
+    for i in range(bit_size):
+        mcb = sum((num >> i) & 1 for num in numbers)
+        if mcb >= len(numbers) // 2:
+            gamma |= (1 << i)
+        else:
+            epsilon |= (1 << i)
+
+    return gamma * epsilon
+
+
+def common(numbers, nth_bit, bit_size=12):
+    mcbits = [[], []]
+    for num in numbers:
+        bit = (num >> (bit_size - 1 - nth_bit)) & 1
+        mcbits[bit].append(num)
+    return mcbits
+
+
+def bit_criteria(numbers, criteria, bit_size=12):
+    numbers = list(numbers)
+
+    for i in range(bit_size):
+        if len(numbers) <= 1:
+            break
+
+        mcbits = common(numbers, i, bit_size)
+        numbers = criteria(mcbits)
+
+    return numbers[0]
+
+
+def oxygen_criteria(mcbits):
+    return mcbits[0] if len(mcbits[0]) > len(mcbits[1]) else mcbits[1]
+
+
+def co2_criteria(mcbits):
+    return mcbits[0] if len(mcbits[0]) <= len(mcbits[1]) else mcbits[1]
+
+
+def part2(bit_size=12):
+    numbers = [int(line, 2) for line in lines]
+
+    oxygen_rate = bit_criteria(numbers, oxygen_criteria, bit_size)
+    co2_rate = bit_criteria(numbers, co2_criteria, bit_size)
+
+    return oxygen_rate * co2_rate
+
+
+if __name__ == "__main__":
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
