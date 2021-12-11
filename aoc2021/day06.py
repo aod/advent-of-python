4cca7654476ec9fe9e6fac56da80be482d09d45f
Oktay <oktay@yatko.dev>
2021-12-11 20:13:41 +0100

    Refactor 2021-6
    Remove `new_fishes` entirely...

--- aoc2021/day06.py
+++ aoc2021/day06.py
@@ -4,17 +4,17 @@ from aocd import data
 
 def simulate(fishies: Dict[int, int], days: int):
     for _ in range(days):
-        new_fishies: Dict[int, int] = {k: 0 for k in range(0, 9)}
-        new_fishies[0] = fishies[1]
-        new_fishies[1] = fishies[2]
-        new_fishies[2] = fishies[3]
-        new_fishies[3] = fishies[4]
-        new_fishies[4] = fishies[5]
-        new_fishies[5] = fishies[6]
-        new_fishies[6] = fishies[0] + fishies[7]
-        new_fishies[7] = fishies[8]
-        new_fishies[8] = fishies[0]
-        fishies = new_fishies
+        fishies = {
+            0: fishies[1],
+            1: fishies[2],
+            2: fishies[3],
+            3: fishies[4],
+            4: fishies[5],
+            5: fishies[6],
+            6: fishies[0] + fishies[7],
+            7: fishies[8],
+            8: fishies[0],
+        }
     return sum(fishies.values())
 
 
